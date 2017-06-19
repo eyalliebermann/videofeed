a9d44599ddc2675eb6d0cfe1cd14e6707957e8d2
eyalliebermann <eyal.liebermann@gmail.com>
2017-06-19 19:04:14 +0300

    first test

---
+++ test/server.spec.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('../server')
+
+
+describe('GET videos', function() {
+  it('respond with json', function(done) {
+    request(app)
+      .get('/v1.0/videos')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+});
