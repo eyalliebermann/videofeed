29ad8899ffb88add92103d114e4d03efa6152b5a
eyalliebermann <eyal.liebermann@gmail.com>
2017-06-21 19:55:26 +0300

    nock nock

--- public/loadFacebookSdk.js
+++ public/loadFacebookSdk.js
@@ -1,10 +1,8 @@
 //Load Facebook SDK for JavaScript as described here https://developers.facebook.com/docs/plugins/embedded-video-player
-
-(function (d, s, id) {
+(function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
+    js = d.createElement(s); js.id = id;
     js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
     fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+  }(document, 'script', 'facebook-jssdk'));
--- test/server.spec.js
+++ test/server.spec.js
@@ -1,5 +1,51 @@
 const request = require('supertest');
 const app = require('../server')
+const nock = require('nock');
+
+
+var FEED =
+{
+  "items": [{
+      "title": "Be a winner!",
+      "type": "video",
+      "source": "youtube",
+      "videoId": "I33u_EHLI3w",
+      "views": 12451409
+    },
+    {
+      "title": "How to prepare a great beer",
+      "type": "video",
+      "source": "facebook",
+      "videoId": "1052114818157758",
+      "views": 4569654
+    },
+    {
+      "type": "video",
+      "source": "url",
+      "url": "http://cdn.playbuzz.com/content/feed/video-1.mp4",
+      "views": 8820
+    },
+    {
+      "title": "Top 10 fastest cars in the world",
+      "type": "video",
+      "source": "youtube",
+      "videoId": "h8MbhS5XKow",
+      "views": 25560867
+    },
+    {
+      "title": "A funny dog barking",
+      "type": "video",
+      "source": "youtube",
+      "videoId": "MveqXxB12YA",
+      "views": 4287171
+    }
+  ]
+};
+
+var playbuzz = nock('https://cdn.playbuzz.com/')
+                .get('/content/feed/items')
+                .times(999)
+                .reply(200, FEED);
 
 
 describe('GET videos', function () {
@@ -17,6 +63,5 @@ describe('GET videos', function () {
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, done);
-
   });
 });
