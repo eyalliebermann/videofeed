4aafa9c9252115e6372fc0ef8a4c0a7b89ac6ef8
eyalliebermann <eyal.liebermann@gmail.com>
2017-06-21 19:21:11 +0300

    extra test

--- test/server.spec.js
+++ test/server.spec.js
@@ -2,12 +2,21 @@ const request = require('supertest');
 const app = require('../server')
 
 
-describe('GET videos', function() {
-  it('respond with json', function(done) {
+describe('GET videos', function () {
+  it('respond with json', function (done) {
     request(app)
       .get('/api/v1.0/videos')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, done);
+  });
+
+  it('respond with bogus filter as usual', function (done) {
+    request(app)
+      .get('/api/v1.0/videos/filter-by/foo')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+
   });
 });
